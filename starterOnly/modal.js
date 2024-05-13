7f358e5296b4ed8354ecc6feddb6a07ae0fe48e9
AureLeeLi <aduynslaeger@gmail.com>
2024-05-13 23:16:07 +0200

    ✨ First Issue fixed : add event listener on span close, to close the modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,9 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
+//span X to close modal
+const modalClose = document.querySelector(".close");
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -19,5 +22,11 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// event listener on span modalClose
+modalClose.addEventListener("click", () => {
+  //if click is detected : function = modalbg {display : none;}
+  modalbg.style.display = "none";
+})
 
 
